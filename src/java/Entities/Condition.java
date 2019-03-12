94bde247db65588ae99104e7de283a0e84e1cbf9
Joakim Andersson <Joakim.Ander@Hotmail.com>
2019-03-12 13:44:41 +0100

    whatever

--- src/java/Entities/Condition.java
+++
@@ -1,5 +0,0 @@
-package Entities;
-
-public enum Condition {
-    MINT, USED, SCRAP;
-}
--- src/java/REST/RestEnabler.java
+++
@@ -1,9 +0,0 @@
-package REST;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestEnabler extends Application {
-    
-}
